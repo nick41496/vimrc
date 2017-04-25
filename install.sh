a2a3d9b6dc026c3f762a701a26abd2ceed54123a
Nick White <nick41496@gmail.com>
2017-04-24 23:21:37 -0400

    Support neovim

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-ln -s .vimrc ~/.vimrc
-mkdir ~/vimundo
+if [ -d ~/.config/nvim ] ; then
+    rm -rf ~/.config/nvim
+    ln -s -T ~/.vim ~/.config/nvim
+    ln -s -T ~/.vim/.vimrc ~/.config/nvim/init.vim
+fi
+ln -s ~/.vim/.vimrc ~/.vimrc
+mkdir ~/.vimundo
 git submodule init
 git submodule update
