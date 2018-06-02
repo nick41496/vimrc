9a07a3304eff51a11602293d1f53ec9ed1c34ef0
Nick White <nick41496@gmail.com>
2018-06-02 14:03:16 -0400

    Remove neovim config

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-if [ -d ~/.config/nvim ] ; then
-    rm -rf ~/.config/nvim
-    ln -s -T ~/.vim ~/.config/nvim
-    ln -s -T ~/.vim/.vimrc ~/.config/nvim/init.vim
-fi
 ln -s ~/.vim/.vimrc ~/.vimrc
 mkdir ~/.vimundo
 git submodule init
